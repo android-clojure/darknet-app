072c2042f0fa4eba5f024c4b5c4d0a3a3f1e6e37
Henry Garner <henry@henrygarner.com>
2016-04-02 17:40:03 +0100

    Remove flashlight and unused preference activity

--- src/java/com/foxdogstudios/peepers/StreamCameraActivity.java
+++ src/java/com/foxdogstudios/peepers/StreamCameraActivity.java
@@ -56,8 +56,6 @@ public final class StreamCameraActivity extends Activity
 
     private static final String PREF_CAMERA = "camera";
     private static final int PREF_CAMERA_INDEX_DEF = 0;
-    private static final String PREF_FLASH_LIGHT = "flash_light";
-    private static final boolean PREF_FLASH_LIGHT_DEF = false;
     private static final String PREF_PORT = "port";
     private static final int PREF_PORT_DEF = 8080;
     private static final String PREF_JPEG_SIZE = "size";
@@ -74,14 +72,10 @@ public final class StreamCameraActivity extends Activity
 
     private String mIpAddress = "";
     private int mCameraIndex = PREF_CAMERA_INDEX_DEF;
-    private boolean mUseFlashLight = PREF_FLASH_LIGHT_DEF;
     private int mPort = PREF_PORT_DEF;
     private int mJpegQuality = PREF_JPEG_QUALITY_DEF;
     private int mPrevieSizeIndex = PREF_PREVIEW_SIZE_INDEX_DEF;
     private TextView mIpAddressView = null;
-    private LoadPreferencesTask mLoadPreferencesTask = null;
-    private SharedPreferences mPrefs = null;
-    private MenuItem mSettingsMenuItem = null;
     private WakeLock mWakeLock = null;
 
     public StreamCameraActivity()
@@ -99,8 +93,6 @@ public final class StreamCameraActivity extends Activity
         
         setContentView(R.layout.stream_camera_activity);
 
-        new LoadPreferencesTask().execute();
-
         mSurfaceView = (SurfaceView) findViewById(R.id.camera);
         mPreviewDisplay = mSurfaceView.getHolder();
         mPreviewDisplay.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -108,7 +100,6 @@ public final class StreamCameraActivity extends Activity
 
         mIpAddress = tryGetIpV4Address();
         mIpAddressView = (TextView) findViewById(R.id.ip_address);
-        updatePrefCacheAndUi();
 
         final PowerManager powerManager =
                 (PowerManager) getSystemService(POWER_SERVICE);
@@ -121,12 +112,6 @@ public final class StreamCameraActivity extends Activity
     {
         super.onResume();
         mRunning = true;
-        if (mPrefs != null)
-        {
-            mPrefs.registerOnSharedPreferenceChangeListener(
-                    mSharedPreferenceListener);
-        } // if
-        updatePrefCacheAndUi();
         tryStartCameraStreamer();
         mWakeLock.acquire();
     } // onResume()
@@ -137,11 +122,6 @@ public final class StreamCameraActivity extends Activity
         mWakeLock.release();
         super.onPause();
         mRunning = false;
-        if (mPrefs != null)
-        {
-            mPrefs.unregisterOnSharedPreferenceChangeListener(
-                    mSharedPreferenceListener);
-        } // if
         ensureCameraStreamerStopped();
     } // onPause()
 
@@ -176,9 +156,9 @@ public final class StreamCameraActivity extends Activity
 
     private void tryStartCameraStreamer()
     {
-        if (mRunning && mPreviewDisplayCreated && mPrefs != null)
+        if (mRunning && mPreviewDisplayCreated)
         {
-            mCameraStreamer = new CameraStreamer(mCameraIndex, mUseFlashLight, mPort,
+            mCameraStreamer = new CameraStreamer(mCameraIndex, false, mPort,
                     mPrevieSizeIndex, mJpegQuality, mPreviewDisplay);
             mCameraStreamer.start();
         } // if
@@ -192,136 +172,6 @@ public final class StreamCameraActivity extends Activity
             mCameraStreamer = null;
         } // if
     } // stopCameraStreamer()
-
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu)
-    {
-        super.onCreateOptionsMenu(menu);
-        mSettingsMenuItem = menu.add(R.string.settings);
-        mSettingsMenuItem.setIcon(android.R.drawable.ic_menu_manage);
-        return true;
-    } // onCreateOptionsMenu(Menu)
-
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item)
-    {
-        if (item != mSettingsMenuItem)
-        {
-            return super.onOptionsItemSelected(item);
-        } // if
-        startActivity(new Intent(this, PeepersPreferenceActivity.class));
-        return true;
-    } // onOptionsItemSelected(MenuItem)
-
-    private final class LoadPreferencesTask
-            extends AsyncTask<Void, Void, SharedPreferences>
-    {
-        private LoadPreferencesTask()
-        {
-            super();
-        } // constructor()
-
-        @Override
-        protected SharedPreferences doInBackground(final Void... noParams)
-        {
-            return PreferenceManager.getDefaultSharedPreferences(
-                    StreamCameraActivity.this);
-        } // doInBackground()
-
-        @Override
-        protected void onPostExecute(final SharedPreferences prefs)
-        {
-            StreamCameraActivity.this.mPrefs = prefs;
-            prefs.registerOnSharedPreferenceChangeListener(
-                    mSharedPreferenceListener);
-            updatePrefCacheAndUi();
-            tryStartCameraStreamer();
-        } // onPostExecute(SharedPreferences)
-
-
-    } // class LoadPreferencesTask
-
-    private final OnSharedPreferenceChangeListener mSharedPreferenceListener =
-            new OnSharedPreferenceChangeListener()
-    {
-        @Override
-        public void onSharedPreferenceChanged(final SharedPreferences prefs,
-                final String key)
-        {
-            updatePrefCacheAndUi();
-        } // onSharedPreferenceChanged(SharedPreferences, String)
-
-    }; // mSharedPreferencesListener
-
-    private final int getPrefInt(final String key, final int defValue)
-    {
-        // We can't just call getInt because the preference activity
-        // saves everything as a string.
-        try
-        {
-            return Integer.parseInt(mPrefs.getString(key, null /* defValue */));
-        } // try
-        catch (final NullPointerException e)
-        {
-            return defValue;
-        } // catch
-        catch (final NumberFormatException e)
-        {
-            return defValue;
-        } // catch
-    } // getPrefInt(String, int)
-
-    private final void updatePrefCacheAndUi()
-    {
-        mCameraIndex = getPrefInt(PREF_CAMERA, PREF_CAMERA_INDEX_DEF);
-        if (hasFlashLight())
-        {
-            if (mPrefs != null)
-            {
-                mUseFlashLight = mPrefs.getBoolean(PREF_FLASH_LIGHT,
-                        PREF_FLASH_LIGHT_DEF);
-            } // if
-            else
-            {
-                mUseFlashLight = PREF_FLASH_LIGHT_DEF;
-            } // else
-        } //if
-        else
-        {
-            mUseFlashLight = false;
-        } // else
-
-        // XXX: This validation should really be in the preferences activity.
-        mPort = getPrefInt(PREF_PORT, PREF_PORT_DEF);
-        // The port must be in the range [1024 65535]
-        if (mPort < 1024)
-        {
-            mPort = 1024;
-        } // if
-        else if (mPort > 65535)
-        {
-            mPort = 65535;
-        } // else if
-
-        mPrevieSizeIndex = getPrefInt(PREF_JPEG_SIZE, PREF_PREVIEW_SIZE_INDEX_DEF);
-        mJpegQuality = getPrefInt(PREF_JPEG_QUALITY, PREF_JPEG_QUALITY_DEF);
-        // The JPEG quality must be in the range [0 100]
-        if (mJpegQuality < 0)
-        {
-            mJpegQuality = 0;
-        } // if
-        else if (mJpegQuality > 100)
-        {
-            mJpegQuality = 100;
-        } // else if
-        mIpAddressView.setText("http://" + mIpAddress + ":" + mPort + "/");
-    } // updatePrefCacheAndUi()
-
-    private boolean hasFlashLight()
-    {
-        return getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA_FLASH);
-    } // hasFlashLight()
 
     private static String tryGetIpV4Address()
     {
@@ -366,4 +216,3 @@ public final class StreamCameraActivity extends Activity
     }
 
 } // class StreamCameraActivity
-
