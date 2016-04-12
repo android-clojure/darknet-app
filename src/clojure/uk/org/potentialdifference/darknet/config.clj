109e8ed1f0af06d4d892cd9b5a4c4bf3fe130c1e
Henry Garner <henry@henrygarner.com>
2016-04-12 10:26:01 +0100

    Set production IP

--- src/clojure/uk/org/potentialdifference/darknet/config.clj
+++ src/clojure/uk/org/potentialdifference/darknet/config.clj
@@ -1,6 +1,8 @@
 (ns uk.org.potentialdifference.darknet.config)
 
+(def ip "192.168.1.24")
+
 (def config
-  {:ws-url "wss://192.168.0.6:8081"
-   :api-url "http://192.168.0.6:8080"
+  {:ws-url  (str "wss://" ip ":8081")
+   :api-url (str "http://") ip ":8080"
    :api-key "x9RHJ2I6nWi376Wa"})
