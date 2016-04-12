3b3cada1244d7f2711ded3609bd7854d52094fdf
Henry Garner <henry@henrygarner.com>
2016-04-12 21:52:38 +0100

    Move server onto new IP

--- src/clojure/uk/org/potentialdifference/darknet/config.clj
+++ src/clojure/uk/org/potentialdifference/darknet/config.clj
@@ -1,8 +1,8 @@
 (ns uk.org.potentialdifference.darknet.config)
 
-(def ip "192.168.1.24")
+(def ip "192.168.1.25")
 
 (def config
   {:ws-url  (str "wss://" ip ":8081")
-   :api-url (str "http://") ip ":8080"
+   :api-url (str "http://" ip ":8080")
    :api-key "x9RHJ2I6nWi376Wa"})
