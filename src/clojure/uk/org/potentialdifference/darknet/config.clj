3961bef00c63b4d28b0911999dde0dc492396e7b
Henry Garner <henry@henrygarner.com>
2016-04-13 23:46:13 +0100

    Prepare for SSL

--- src/clojure/uk/org/potentialdifference/darknet/config.clj
+++ src/clojure/uk/org/potentialdifference/darknet/config.clj
@@ -1,8 +1,10 @@
 (ns uk.org.potentialdifference.darknet.config)
 
-(def ip "192.168.1.25")
+(def host "192.168.1.25")
+;; (def host "192.168.2.24") ;; DELETEME
 
 (def config
-  {:ws-url  (str "wss://" ip ":8081")
-   :api-url (str "http://" ip ":8080")
+  {:api-host host
+   :ws-url  (str "wss://" host ":8081")
+   :api-url (str "http://" host ":8080")
    :api-key "x9RHJ2I6nWi376Wa"})
--- src/clojure/uk/org/potentialdifference/darknet/server.clj
+++ src/clojure/uk/org/potentialdifference/darknet/server.clj
@@ -1,7 +1,12 @@
 (ns uk.org.potentialdifference.darknet.server
   (:require [happy.core :as h]
             [happy.client.okhttp :as ok]
-            [uk.org.potentialdifference.darknet.config :refer [config]]))
+            [uk.org.potentialdifference.darknet.config :refer [config]])
+  (:import [java.security.cert CertificateFactory]
+           [javax.net.ssl TrustManagerFactory]
+           [java.security KeyStore]
+           [javax.net.ssl SSLContext]
+           [javax.net.ssl HostnameVerifier]))
 
 (defn stream-video [from to width height]
   (let [api-key (get config :api-key)
@@ -27,3 +32,25 @@
      :client (ok/create)
      :handler (fn [response]
                 (f (:body response)))}))
+
+(defn ssl-client! [client context]
+  (let [cf (CertificateFactory/getInstance "X.509")
+        stream (-> context (.getResources) (.openRawResource "server_cert.cert"))
+        ca (try (.generateCertificate stream)
+                (finally
+                  (.close stream)))
+        key-store (KeyStore/getInstance (KeyStore/getDefaultType))
+        trust-factory (TrustManagerFactory/getInstance (TrustManagerFactory/getDefaultAlgorithm))
+        ssl-context (SSLContext/getInstance "TLS")]
+    (.load key-store nil nil)
+    (.setCertificateEntry "ca" ca)
+    (.init trust-factory key-store)
+    (.init ssl-context nil (.getTrustManagers trust-factory) nil)
+    (.setSslSocketFactory (.getSocketFactory ssl-context))
+    (.setHostnameVerifier (reify HostnameVerifier
+                            (verify [this host session]
+                              (= host (:host config)))))))
+
+(defn client [context]
+  (doto (ok/create)
+    (ssl-client! context)))
